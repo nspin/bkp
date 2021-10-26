58be1d2ba29fde4a4f8003c30b60c7da05b4fdf9
Nick Spinale <nick@nickspinale.com>
2021-10-26 13:49:59 +0000

    Organize top-level imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,34 @@
 #![feature(exit_status_error)]
 #![feature(iter_intersperse)]
 
-pub use blob::{BlobShadow, BlobShadowContentSh256};
-pub use blob_store::{sha256sum, FilesystemRealBlobStorage, MockRealBlobStorage, RealBlobStorage};
-pub use cli::cli_main;
-pub use database::{
-    Database, TraversalCallbacks, Traverser, Visit, VisitBlob, VisitLink, VisitTree,
-    VisitTreeDecision,
-};
-pub use paths::{BulkPath, BulkPathComponent, BulkTreeEntryName};
-pub use snapshot::{Snapshot, SnapshotEntries, SnapshotEntry, SnapshotEntryValue};
-
 mod paths;
 mod blob;
 mod blob_store;
 mod snapshot;
 mod database;
 mod cli;
+
+#[rustfmt::skip]
+pub use crate::{
+    paths::{
+        BulkPath, BulkPathComponent, BulkTreeEntryName,
+    },
+    blob::{
+        BlobShadow, BlobShadowContentSh256,
+    },
+    blob_store::{
+        RealBlobStorage, FilesystemRealBlobStorage, MockRealBlobStorage,
+        sha256sum,
+    },
+    snapshot::{
+        Snapshot, SnapshotEntries, SnapshotEntry, SnapshotEntryValue,
+    },
+    database::{
+        Database,
+        TraversalCallbacks, Traverser,
+        Visit, VisitBlob, VisitLink, VisitTree, VisitTreeDecision,
+    },
+    cli::{
+        cli_main,
+    },
+};
