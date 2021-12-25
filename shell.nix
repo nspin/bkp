e847186c856f28e9ebb9a6e4c01bed03d5a07228
Nick Spinale <nick@nickspinale.com>
2021-12-25 14:50:18 +0000

    Extend nix-shell with darwin support

--- shell.nix
+++ shell.nix
@@ -1,9 +1,17 @@
-with import <nixpkgs> {};
+with import (builtins.fetchGit {
+  url = "https://github.com/NixOS/nixpkgs.git";
+  ref = "master";
+  rev = "732717d4042b4062840868ef28ca7c19abfbfc0b";
+}) {};
 
 mkShell {
   nativeBuildInputs = [
+    rustup
     pkg-config
     openssl
     fuse
+  ] ++ lib.optionals stdenv.isDarwin [
+    darwin.Security
+    libiconv
   ];
 }
